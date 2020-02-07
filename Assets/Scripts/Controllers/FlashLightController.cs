eecd18639750e28294c3c852ec8d4e8ee83a8964
Shagrid <shagrid@yandex.ru>
2020-02-07 11:37:53 +0300

    Реализован код первого урока, модернизация фонарика

---
+++ Assets/Scripts/Controllers/FlashLightController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using Views;
+
+namespace Geekbrains
+{
+    public sealed class FlashLightController : BaseController, IExecute, IInitialization
+    {
+        private FlashLightModel _flashLightModel;
+        private FlashLightUiBar _flashLightUiBar;
+
+        public void Initialization()
+        {
+            _flashLightModel = Object.FindObjectOfType<FlashLightModel>();
+            _flashLightUiBar = Object.FindObjectOfType<FlashLightUiBar>();
+        }
+
+        public override void On()
+        {
+            if(IsActive) return;
+            if (_flashLightModel.BatteryChargeCurrent <= 0) return;
+            base.On();
+            _flashLightModel.Switch(FlashLightActiveType.On);
+        }
+
+        public override void Off()
+        {
+            if (!IsActive) return;
+            base.Off();
+            _flashLightModel.Switch(FlashLightActiveType.Off);
+        }
+
+        public void Execute()
+        {
+            if (!IsActive && _flashLightModel.BatteryFull())
+            {
+                return;
+            }
+            
+
+            _flashLightModel.Rotation();
+            if (_flashLightModel.EditBatteryCharge())
+            {
+                _flashLightUiBar.Fill = _flashLightModel.BatteryChargeCurrent/_flashLightModel.BatteryChargeMax;
+                _flashLightUiBar.SetColor(_flashLightModel.LowBattery() ? Color.red : Color.yellow);
+            }
+            else
+            {
+                Off();
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Models/FlashLightModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+namespace Geekbrains
+{
+    public sealed class FlashLightModel : BaseObjectScene
+    {
+        private Light _light;
+        private Transform _goFollow;
+        private Vector3 _vecOffset;
+        private float _lowBatteryRatio = 4;
+        public float BatteryChargeCurrent { get; private set; }
+        [SerializeField] private float _battaryChargeSpeed = 4;
+        [SerializeField] private float _batteryChargeMax = 10;
+        [SerializeField] private float _speed = 11;
+        public float BatteryChargeMax => _batteryChargeMax;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _light = GetComponent<Light>();
+            _goFollow = Camera.main.transform;
+            _vecOffset = transform.position - _goFollow.position;
+            BatteryChargeCurrent = BatteryChargeMax;
+        }
+
+        public void Switch(FlashLightActiveType value)
+        {
+            switch (value)
+            {
+                case FlashLightActiveType.On:
+                    _light.enabled = true;
+                    transform.position = _goFollow.position + _vecOffset;
+                    transform.rotation = _goFollow.rotation;
+                    break;
+                case FlashLightActiveType.None:
+                    break;
+                case FlashLightActiveType.Off:
+                    _light.enabled = false;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+        }
+
+        public void Rotation()
+        {
+            transform.position = _goFollow.position + _vecOffset;
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+                _goFollow.rotation, _speed * Time.deltaTime);
+        }
+
+        public bool EditBatteryCharge()
+        {
+            if (BatteryChargeCurrent > 0 && _light.enabled)
+            {
+                BatteryChargeCurrent -= Time.deltaTime;
+                return true;
+            }
+            if (BatteryChargeCurrent < _batteryChargeMax && !_light.enabled)
+            {
+                BatteryChargeCurrent += Time.deltaTime / _battaryChargeSpeed;
+                if (BatteryChargeCurrent > BatteryChargeMax)
+                {
+                    BatteryChargeCurrent = BatteryChargeMax;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool LowBattery()
+        {
+            return BatteryChargeCurrent <= BatteryChargeMax / _lowBatteryRatio;
+        }
+
+        public bool BatteryFull()
+        {
+            return BatteryChargeCurrent == BatteryChargeMax;
+        }
+    }
+}
